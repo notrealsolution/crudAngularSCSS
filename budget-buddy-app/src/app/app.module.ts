048f9f98c77e9cf7de419c89154e2181554bfe0b
Daniel Antonio Sánchez Jiménez <notrealsolution@gmail.com>
2024-06-26 10:13:50 -0600

    Creación de formulario para agregar un nuevo gasto

--- budget-buddy-app/src/app/app.module.ts
+++ budget-buddy-app/src/app/app.module.ts
@@ -4,17 +4,21 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import { ExpenseFormComponent } from './components/expense-form/expense-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ExpenseListComponent,
+    ExpenseFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ budget-buddy-app/src/app/components/expense-form/expense-form.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ExpenseService } from '../../services/expense.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-expense-form',
+  templateUrl: './expense-form.component.html',
+  styleUrls: ['./expense-form.component.scss']
+})
+export class ExpenseFormComponent {
+  expenseForm: FormGroup = new FormGroup({
+    type: new FormControl('',[Validators.required, Validators.maxLength(100)]),
+    amount: new FormControl('',[Validators.required]),
+    description: new FormControl('',[Validators.required]),
+    expense_date: new FormControl('',[Validators.required]),
+  });
+
+  constructor(
+    private expenseService:ExpenseService,
+    private router: Router
+  ){}
+  saveExpense(): void {
+    this.expenseService.createExpense(this.expenseForm.value).subscribe( expense => {
+      this.router.navigateByUrl('/gastos');
+    })
+  }
+  hasError(field: string): boolean {
+    const errorsObject = this.expenseForm.get(field)?.errors ?? {};
+    const errors = Object.keys(errorsObject);
+    if( errors.length && (this.expenseForm.get(field)?.touched || this.expenseForm.get(field)?.dirty) ){
+      return true;
+    }
+    return false;
+  }
+  getCurrentError( field: string ):string {
+    const errorsObject = this.expenseForm.get(field)?.errors ?? {};
+    const errors = Object.keys(errorsObject);
+    if(!errors) return '';
+    return errors[0]
+  }
+}
